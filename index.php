1ffdf0247818bf3927864e204fe61d6e5d9dbca9
danyal-903 <danyalr8@gmail.com>
2022-12-27 16:39:13 +0500

    details updated

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
 require __DIR__ . '/vendor/autoload.php';
 
 
@@ -14,9 +7,6 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__ . '/src' . $uri)) {
     return false;
 }
